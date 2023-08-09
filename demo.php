0be6e556d8d6c949878296402fe8054b37e57263
uc-sag <sagar.gupta@ucertify.com>
2023-08-09 09:04:24 +0530

    Top Functions Practice

---
+++ demo.php
@@ -0,0 +1,47 @@
+<?php
+ 
+    require_once './pe-gold3/prepengine-header.php' ;
+    
+    // $email = "sagar#~<.gupta@ucertify.com";
+    // echo isValidEmail($email);    //it does not print anything
+
+    // $email = "sagar.gupta123@ucertify.com";
+    // echo isValidEmail($email);       //it print true
+
+    // $email = 'sagar<></>gupta@ucertify.com';
+    // echo sanitizeEmail($email);         //output - sagargupta@ucertify.com
+
+    //  echo gen_uuid(8,'saltadded');      //it generates the unique ID
+
+    // $datefrom = "2019-01-01";
+    // $dateto = "2019-02-01";
+    // $interval = "d";
+    // echo datediff($interval,$datefrom,$dateto);
+
+    // $xml = "<?xml >   <note>     <to>Tove</to>    <from>Jani</from>    <heading>Reminder</heading>    <body>Don't forget me this weekend!</body>  </note>  ";
+    // print_r(xml2array($xml));
+
+    // $arr = '[1,2,3,"dad"]';
+    // print_r(xml2array($arr));
+
+    // $arr = [1,2,3,4,5];
+    // $isDie = 1;
+    // $isPre =1;
+
+    // __ucd($arr,$isDie,$isPre);   
+ 
+    // echo 'next line';
+
+    // $arr1 = ['a','b','c','d','e'];
+    // $arr2 = [1,2,3,4,5];
+    // $recursive = false;
+    // $newArr = array_merge_uc($arr1,$arr2,true);
+    // print_r($newArr);
+
+    // $time = strtotime('2019-06-13');
+    // echo friendlyDate($time);
+
+    
+
+
+?>
